a872192c22d0170d9bb5f23486e159cceb4799ac
aditydp90625 <136080950+aditydp90625@users.noreply.github.com>
2024-11-30 19:03:18 +0000

    Update HSV_CW_Task5.thy

--- HSV_CW_Task5.thy
+++ HSV_CW_Task5.thy
@@ -1,16 +1,18 @@
-section \<open> Task 5: Verifying a naive SAT solver. \<close>
+theory HSV_CW_Task5 imports Main begin
 
-text \<open> This function can be used with List.fold to simulate a do-until loop. \<close>
-definition until :: "('a \<Rightarrow> bool) \<Rightarrow> 'a \<Rightarrow> 'a option \<Rightarrow> 'a option" 
+section ‹ Task 5: Verifying a naive SAT solver. ›
+
+text ‹ This function can be used with List.fold to simulate a do-until loop. ›
+definition until :: "('a ⇒ bool) ⇒ 'a ⇒ 'a option ⇒ 'a option" 
   where
-  "until p x z == if z = None then if p x then Some x else None else z" 
+  "until p x z ≡ if z = None then if p x then Some x else None else z" 
 
-text \<open> Once the loop condition holds, the return value is fixed. \<close>
+text ‹ Once the loop condition holds, the return value is fixed. ›
 lemma until_some: "fold (until p) xs (Some z) = Some z"
   by (induct xs, auto simp add: until_def)
 
-text \<open> If the loop returns None, the condition holds for no element of the input list. \<close>
-lemma until_none: "fold (until p) xs None = None \<Longrightarrow> list_all (\<lambda>x. \<not> p x) xs"
+text ‹ If the loop returns None, the condition holds for no element of the input list. ›
+lemma until_none: "fold (until p) xs None = None ⟹ list_all (λx. ¬ p x) xs"
 proof (induct xs)
   case Nil
   thus ?case by simp
@@ -24,17 +26,17 @@ next
     hence False using until_some by (metis option.distinct(1))
   } 
   moreover {
-    assume "\<not> p a"
+    assume "¬ p a"
     hence "until p a None = None" by (simp add: until_def)
     hence "fold (until p) xs None = None" using * by presburger
-    hence "list_all (\<lambda>x. \<not> p x) xs" by (rule Cons.hyps)
-    hence ?case by (simp add: `\<not> p a`)
+    hence "list_all (λx. ¬ p x) xs" by (rule Cons.hyps)
+    hence ?case by (simp add: `¬ p a`)
   } 
   ultimately show ?case by blast
 qed
 
-text \<open> If the loop returns Some x, the condition holds for element x of the input list. \<close>
-lemma until_none_some: "fold (until p) xs None = Some x \<Longrightarrow> p x \<and> List.member xs x"
+text ‹ If the loop returns Some x, the condition holds for element x of the input list. ›
+lemma until_none_some: "fold (until p) xs None = Some x ⟹ p x ∧ List.member xs x"
 proof (induct xs)
   case Nil
   thus ?case by simp
@@ -45,112 +47,120 @@ next
     assume "p a"
     hence "until p a None = Some a" by (simp add: until_def) 
     hence "a = x" by (metis * option.inject until_some)
-    hence "p x \<and> List.member (a # xs) x" using `p a` in_set_member by force
+    hence "p x ∧ List.member (a # xs) x" using `p a` in_set_member by force
   } 
   moreover {
-    assume "\<not> p a"
+    assume "¬ p a"
     hence "until p a None = None" by (simp add: until_def)
     hence "fold (until p) xs None = Some x" using * by presburger
-    hence "p x \<and> List.member (a # xs) x" using Cons.hyps by (simp add: member_rec(1))
+    hence "p x ∧ List.member (a # xs) x" using Cons.hyps by (simp add: member_rec(1))
   } 
   ultimately show ?case by blast
 qed
 
-text \<open> We shall use strings to represent symbols. \<close>
+text ‹ We shall use strings to represent symbols. ›
 type_synonym symbol = "string"
 
-text \<open> A literal is either a variable or a negated symbol. \<close>
-type_synonym literal = "symbol * bool"
+text ‹ A literal is either a variable or a negated symbol. ›
+type_synonym literal = "symbol × bool"
 
-text \<open> A valuation is a list of symbols and their truth values. \<close>
-type_synonym valuation = "(symbol * bool) list"
+text ‹ A valuation is a list of symbols and their truth values. ›
+type_synonym valuation = "(symbol × bool) list"
 
-text \<open> A clause is a disjunction of literals. \<close>
+text ‹ A clause is a disjunction of literals. ›
 type_synonym clause = "literal list"
 
-text \<open> A SAT query is a conjunction of clauses. \<close>
+text ‹ A SAT query is a conjunction of clauses. ›
 type_synonym query = "clause list"
 
-text \<open> Given a valuation, evaluate a clause to its truth value. \<close>
-definition evaluate_clause :: "valuation \<Rightarrow> clause \<Rightarrow> bool"
+text ‹ Given a valuation, evaluate a clause to its truth value. ›
+definition evaluate_clause :: "valuation ⇒ clause ⇒ bool"
 where 
-  "evaluate_clause \<rho> c = list_ex (List.member \<rho>) c"
+  "evaluate_clause ρ c = list_ex (List.member ρ) c"
 
-text \<open> Given a valuation, evaluate a query to its truth value. \<close>
-definition evaluate :: "query \<Rightarrow> valuation \<Rightarrow> bool"
+text ‹ Given a valuation, evaluate a query to its truth value. ›
+definition evaluate :: "query ⇒ valuation ⇒ bool"
 where 
-  "evaluate q \<rho> = list_all (evaluate_clause \<rho>) q"
+  "evaluate q ρ = list_all (evaluate_clause ρ) q"
 
-text \<open> Some sample queries and valuations. \<close>
-(* q1 is (a \<or> b) \<and> (\<not>b \<or> c) *)
-definition "q1 == [[(''a'', True), (''b'', True)], [(''b'', False), (''c'', True)]]"
-(* q2 is (a \<or> b) \<and> (\<not>a \<or> \<not>b) *)
-definition "q2 == [[(''a'', True), (''b'', True)], [(''a'', False)], [(''b'', False)]]"
-(* q3 is (a \<or> \<not>b) *)
-definition "q3 == [[(''a'', True), (''b'', False)]]"
-(* q4 is (\<not>b \<or> a) *)
-definition "q4 == [[(''b'', False), (''a'', True)]]"
-definition "\<rho>1 == [(''a'', True), (''b'', True), (''c'', False)]"
-definition "\<rho>2 == [(''a'', False), (''b'', True), (''c'', True)]"
+text ‹ Some sample queries and valuations. ›
+(* q1 is (a ∨ b) ∧ (¬b ∨ c) *)
+definition "q1 ≡ [[(''a'', True), (''b'', True)], [(''b'', False), (''c'', True)]]"
+(* q2 is (a ∨ b) ∧ (¬a ∨ ¬b) *)
+definition "q2 ≡ [[(''a'', True), (''b'', True)], [(''a'', False)], [(''b'', False)]]"
+(* q3 is (a ∨ ¬b) *)
+definition "q3 ≡ [[(''a'', True), (''b'', False)]]"
+(* q4 is (¬b ∨ a) *)
+definition "q4 ≡ [[(''b'', False), (''a'', True)]]"
+definition "ρ1 ≡ [(''a'', True), (''b'', True), (''c'', False)]"
+definition "ρ2 ≡ [(''a'', False), (''b'', True), (''c'', True)]"
 
-value "evaluate q1 \<rho>1" 
-value "evaluate q1 \<rho>2"
+value "evaluate q1 ρ1" 
+value "evaluate q1 ρ2"
 
-text \<open> Construct the list of all possible valuations over the given symbols. \<close>
-fun mk_valuation_list :: "symbol list \<Rightarrow> valuation list"
+text ‹ Construct the list of all possible valuations over the given symbols. ›
+fun mk_valuation_list :: "symbol list ⇒ valuation list"
 where 
   "mk_valuation_list [] = [[]]"
 | "mk_valuation_list (x # xs) = (
-     let \<rho>s = mk_valuation_list xs in 
-     map ((#) (x, True)) \<rho>s @ map ((#) (x, False)) \<rho>s)"
+     let ρs = mk_valuation_list xs in 
+     map ((#) (x, True)) ρs @ map ((#) (x, False)) ρs)"
 
 value "mk_valuation_list [''a'',''b'']"
 value "mk_valuation_list [''a'',''b'',''c'']"
 
-fun symbol_of_literal :: "literal \<Rightarrow> symbol"
+fun symbol_of_literal :: "literal ⇒ symbol"
 where
   "symbol_of_literal (x, _) = x"
 
-text \<open> Extract the list of symbols from the given clause. \<close>
-definition symbol_list_clause :: "clause \<Rightarrow> symbol list"
+text ‹ Extract the list of symbols from the given clause. ›
+definition symbol_list_clause :: "clause ⇒ symbol list"
 where 
-  "symbol_list_clause c == remdups (map symbol_of_literal c)"
+  "symbol_list_clause c ≡ remdups (map symbol_of_literal c)"
 
-text \<open> Extract the list of symbols from the given query. \<close>
-definition symbol_list :: "query \<Rightarrow> symbol list"
+text ‹ Extract the list of symbols from the given query. ›
+definition symbol_list :: "query ⇒ symbol list"
 where
-  "symbol_list q == remdups (concat (map symbol_list_clause q))"
+  "symbol_list q ≡ remdups (concat (map symbol_list_clause q))"
 
 value "symbol_list q1"
 value "symbol_list q2"
 
-text \<open> A naive SAT solver. It works by constructing the list of all
+text ‹ A naive SAT solver. It works by constructing the list of all
   possible valuations over the symbols that appear in the query, and
   then iterating through that list until it finds the first valuation
   that makes the query true. If none of the valuations make the query
-  true, it returns None. \<close>
-definition naive_solve :: "query \<Rightarrow> valuation option"
+  true, it returns None. ›
+definition naive_solve :: "query ⇒ valuation option"
 where
-  "naive_solve q == 
+  "naive_solve q ≡ 
   let xs = symbol_list q in 
-  let \<rho>s = mk_valuation_list xs in
-  List.fold (until (evaluate q)) \<rho>s None"
+  let ρs = mk_valuation_list xs in
+  List.fold (until (evaluate q)) ρs None"
 
 value "naive_solve q1"
 value "naive_solve q2"
 value "naive_solve q3"
 value "naive_solve q4"
 
-text \<open> If the naive SAT solver returns a valuation, then that 
-  valuation really does make the query true. \<close>
+text ‹ If the naive SAT solver returns a valuation, then that 
+  valuation really does make the query true. ›
 theorem naive_solve_correct_sat:
-  assumes "naive_solve q = Some \<rho>"
-  shows "evaluate q \<rho>"
-  oops
+  assumes "naive_solve q = Some ρ"
+  shows "evaluate q ρ" using until_none_some
+    by (metis assms naive_solve_def)
 
-text \<open> If the naive SAT solver returns no valuation, then none of the valuations 
-  it tried make the query true. \<close>
+text ‹ If the naive SAT solver returns no valuation, then none of the valuations 
+  it tried make the query true. ›
 theorem naive_solve_correct_unsat:
   assumes "naive_solve q = None"
-  shows "\<forall>\<rho> \<in> set (mk_valuation_list (symbol_list q)). \<not> evaluate q \<rho>" 
-  oops
+  shows "∀ρ ∈ set (mk_valuation_list (symbol_list q)). ¬ evaluate q ρ"
+proof
+  assume "naive_solve q = None"
+  then have "List.fold (until (evaluate q)) (mk_valuation_list (symbol_list q)) None = None" unfolding naive_solve_def
+    by simp
+  then have "∀ρ ∈ set (mk_valuation_list (symbol_list q)). ¬ evaluate q ρ" 
+qed
+
+
+end
